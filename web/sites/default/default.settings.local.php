4f200b956d31358a891df80845a8a01fc682a867
Allan Chappell <generalredneck@gmail.com>
2019-12-26 09:05:41 -0600

    adding local settings defaults

---
+++ web/sites/default/default.settings.local.php
@@ -0,0 +1,10 @@
+<?php
+$databases['default']['default'] = array(
+  'driver' => 'mysql',
+  'database' => 'drupal7',
+  'username' => 'drupal7',
+  'password' => 'drupal7',
+  'host' => 'database',
+  'charset' => 'utf8mb4',
+  'collation' => 'utf8mb4_general_ci',
+);
